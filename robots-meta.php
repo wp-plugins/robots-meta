19b35ba3edb9bcb4a92f8f43152c9f830580319f
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 13:23:50 +0000

    Closing down the plugin for real
    git-svn-id: https://plugins.svn.wordpress.org/robots-meta/trunk@1199354 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- robots-meta.php
+++ robots-meta.php
@@ -8,9 +8,10 @@ Version: 3.4
 Author URI: http://yoast.com/
 */
 
-if ( is_admin() && current_user_can('manage_options') ) {
-	function robots_meta_upgrade_warning() {
+function robots_meta_upgrade_warning() {
+	if ( is_admin() && current_user_can( 'manage_options' ) ) {
 		echo '<div class="updated" id="message">Robots Meta has been discontinued and isn\'t actively maintained. Please <a href="http://local.wordpress.dev/wp-admin/plugin-install.php?tab=search&s=yoast+seo">install Yoast SEO</a> as that\'s both well maintained and more secure, and disable this plugin.</div>';
 	}
-	add_action( 'admin_notices', 'robots_meta_upgrade_warning' );
 }
+
+add_action( 'admin_notices', 'robots_meta_upgrade_warning' );
